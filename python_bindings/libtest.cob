66586f5327fed8fe7f3d0c076c7e6c8a8d17311f
Gustavo S. Teixeira <gsteixei@gmail.com>
2021-07-16 00:27:37 -0300

    py bindings passing float and double as pointers

--- python_bindings/libtest.cob
+++ python_bindings/libtest.cob
@@ -43,6 +43,54 @@
         END PROGRAM change_string.
 
         *> ********************************
+        IDENTIFICATION DIVISION.
+            PROGRAM-ID. ret_float_pointer.
+            *> takes a float by pointer, then change it.
+        DATA DIVISION.
+            WORKING-STORAGE SECTION.
+                01 answer             PIC 99V99 VALUE ZEROS.
+                01 formated_number    PIC 99V99 VALUE ZEROS.
+                01 float_from_python  USAGE COMP-1 BASED.
+            LINKAGE SECTION.
+                01 pointer_argument USAGE POINTER.
+        PROCEDURE DIVISION USING BY VALUE pointer_argument.
+            SET ADDRESS OF float_from_python TO pointer_argument.
+            display "1- input arg  " float_from_python.
+            *> convert to 99v99 format
+            MOVE float_from_python TO formated_number.
+            DISPLAY "2- formated   " formated_number.
+            *> multiply by two.
+            COMPUTE answer = 2 * formated_number
+            MOVE answer TO float_from_python.
+            display "3- output val " float_from_python.
+            GOBACK.
+        END PROGRAM ret_float_pointer.
+
+        *> ********************************
+        IDENTIFICATION DIVISION.
+            PROGRAM-ID. ret_double_pointer.
+            *> takes a double by pointer, then change it.
+        DATA DIVISION.
+            WORKING-STORAGE SECTION.
+                01 answer             PIC 99V99 VALUE ZEROS.
+                01 formated_number    PIC 99V99 VALUE ZEROS.
+                01 double_from_python  USAGE COMP-2 BASED.
+            LINKAGE SECTION.
+                01 pointer_argument USAGE POINTER.
+        PROCEDURE DIVISION USING BY VALUE pointer_argument.
+            SET ADDRESS OF double_from_python TO pointer_argument.
+            display "1- input arg  " double_from_python.
+            *> convert to 99v99
+            MOVE double_from_python TO formated_number.
+            DISPLAY "2- formated   " formated_number.
+            *> multiply by two.
+            COMPUTE answer = 2 * formated_number
+            MOVE answer TO double_from_python.
+            display "3- output val " double_from_python.
+            GOBACK.
+        END PROGRAM ret_double_pointer.
+
+        *> ********************************
         *> Return an integer
         IDENTIFICATION DIVISION.
             PROGRAM-ID. ret_integer.
--- python_bindings/test.py
+++ python_bindings/test.py
@@ -1,39 +1,59 @@
 # Basic operations with Python bindings interfacing COBOL
-import ctypes
 import pathlib
+from ctypes import byref, c_float, c_double, c_char_p, c_char, c_int, CDLL
 
 if __name__ == "__main__":
-    # Load the shared library
+    # load the shared library
     libname = pathlib.Path().absolute() / "libtest.so"
-    libcob = ctypes.CDLL(libname)
+    libcob = CDLL(libname)
 
-    #################################
+    print('###### INTEGER FROM COBOL ######')
     # 1 - get an integer from cobol
     x = libcob.ret_integer()
     print("Integer", x, type(x))
 
     #################################
-    # 2 - numeric_arguments
-    #    pass numbers as arguments. Note that due limitations of gnucobol, 
-    #    numeric arguments must be passed as strings
-    libcob.string_arguments.argtypes = [ctypes.c_int, ctypes.c_float ]
-    libcob.numeric_arguments.restype = ctypes.c_int
-    a, b = "4", "2"
-    x = libcob.numeric_arguments(a, b)
-    print("Numeric Arguments", x, type(x), "called with", a, b)
+    # 2 - float arguments
+    print('########### FLOAT ##############')
+    libcob.ret_float_pointer.restype = c_float
+    x = c_float(1.1)
+    print('input ', byref(x), x.value)
+    libcob.ret_float_pointer(byref(x))
+    print('output ', x, x.value, type(x))
+
+    #################################
+    # 2 - double argument
+    print('########### DOUBLE #############')
+    libcob.ret_double_pointer.restype = c_double
+    x = c_double(1.1)
+    print('input ', byref(x), x.value)
+    libcob.ret_double_pointer(byref(x))
+    print('output ', x, x.value, type(x))
 
     #################################
     # 3 - This shows how to change a string passed by python as pointer
+    print('########### STRING #############')
     s = "Hello from Python!"
-    p = ctypes.c_char_p(s.encode('utf8'))
-    libcob.change_string.argtypes = [ctypes.c_char_p]
+    p = c_char_p(s.encode('utf8'))
+    libcob.change_string.argtypes = [c_char_p]
     libcob.change_string(p)
     print('COBOL  said:', p.value.decode())
 
     #################################
+    print('######### STRING ARGS ##########')
     # 4 - String arguments
     print("String Arguments")
     s = b'World'
-    p = ctypes.c_char_p(s)
-    libcob.string_arguments.argtypes = [ctypes.c_char_p]
+    p = c_char_p(s)
+    libcob.string_arguments.argtypes = [c_char_p]
     x = libcob.string_arguments(p)
+
+    #################################
+    print('####### NUMBER ARGS (str) ##########')
+    # 5 - numeric_arguments
+    #    pass numbers as string arguments.
+    libcob.string_arguments.argtypes = [c_int, c_float ]
+    libcob.numeric_arguments.restype = c_int
+    a, b = "4", "2"
+    x = libcob.numeric_arguments(a, b)
+    print("Numeric Arguments", x, type(x), "called with", a, b)
